c94ec419955736bb8964d975569fed80235362c6
William <nearata@protonmail.com>
2024-05-09 11:49:20 +0200

    init

---
+++ src/main/java/com/github/neapovil/autofish/AutoFish.java
@@ -0,0 +1,16 @@
+package com.github.neapovil.autofish;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class AutoFish implements ClientModInitializer
+{
+    public static final Logger LOGGER = LoggerFactory.getLogger("AutoFish");
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
